e32d5de4a3c005bd2f59a62605bb4d9e16487091
Jacob Lambert <jacob.lambert@gmail.com>
2019-11-24 23:30:11 -0600

    Add option to quickly create files for new weeks

--- advent.sh
+++ advent.sh
@@ -2,7 +2,8 @@
 
 if [ $# -eq 0 ]
 then
-  echo "Usage: ./advent.sh <day number>"
+  echo "Usage: ./advent.sh day [-f]"
+  echo "-f: creates the folder for the given day from a template"
   exit 1
 fi
 
@@ -10,7 +11,18 @@ RED="\e[31m"
 GREEN="\e[32m"
 
 echo "DAY $1"
+DIR=$(printf "day%02d" $1)
 
-cd $(printf "day%02d" $1)
+if [[ $# -gt 1 && "$2" == "-f" ]]; then
+  [ -d $DIR ] && echo "Directory already exists!" && exit 1
+  cp -r day00 $DIR
+  mv $DIR/src/day00 $DIR/src/$DIR
+  grep -rlI "day00" $DIR | xargs sed -i .sed "s/day00/$DIR/g"
+  find $DIR -name "*.sed" -delete
+  echo "Directory created!"
+  exit 0
+fi
+
+cd $DIR || exit 1
 [ -f Cargo.toml ] && printf "$RED\nRUST:\n" && cargo fmt && cargo run --quiet
-[ -f project.clj ] && printf "$GREEN\nCLOJURE:\n" && lein run
+[ -f project.clj ] && printf "$GREEN\nCLOJURE\n" && lein run
