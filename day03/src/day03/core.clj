6102c008fffa94a65d18334ec27d3d640fb9de30
Jacob Lambert <jacob.lambert@gmail.com>
2019-12-03 22:40:15 -0500

    Correct Day3 clojure solution

--- day03/src/day03/core.clj
+++ day03/src/day03/core.clj
@@ -1,68 +1,51 @@
-;; This solution is incorrect
-
 (ns day03.core
   (:gen-class))
 
 (require '[clojure.string :as str] '[clojure.set :as hashset])
+  
+(defn new-points
+  [[x y] dir len]
+    (case dir
+      \R (for [i (map inc (range x (+ x len)))] [i y])
+      \L (for [i (map dec (range x (- x len) -1))] [i y])
+      \U (for [j (map inc (range y (+ y len)))] [x j])
+      \D (for [j (map dec (range y (- y len) -1))] [x j])))
 
-(def directions {\U [1 0] \D [-1 0] \R [0 1] \L [0 -1]})
-
-(defn section->locations
-   [section pos steps]
-            (for [i (range 1 (inc (:len section)))]
-            [(map + (map #(* % i) (:dir section)) pos) (+ i steps)]))
-
-(defn reduce-section
-      [acc section]
-      (let [locs (section->locations section (:pos acc) (:steps acc))
-            [pos steps] (last locs)]
-            {:locs (into (:locs acc) locs) :pos pos :steps steps}))
-
-(defn wire->locations
-   [wire]
-   (:locs (reduce reduce-section
-                  {:locs {} :pos [0 0] :steps 0}
-                  wire)))
-
-(defn wire->locset
-   [wire]
-   (into #{} (map first (wire->locations wire))))
-
-(defn wire-intersection
-   [w1 w2]
-   (hashset/intersection (wire->locset w1) (wire->locset w2)))
-
-(defn string->section
-     [string]
-     {:dir (directions (first string))
-      :len (read-string (apply str (rest string)))})
+(defn add-section
+  [wire [dir & len-chars]]
+    (let [len (read-string (apply str len-chars))]
+      (concat wire (new-points (last wire) dir len))))
 
 (defn string->wire
-    [string]
-    (let [substrings (str/split string #",")]
-         (map string->section substrings)))
+  [string]
+  (reduce add-section
+          [[0 0]]
+          string))
 
 (def wires
   (->> (slurp "input.txt")
        str/split-lines
+       (map #(str/split % #","))
        (map string->wire)))
 
 (defn abs [n] (max n (- n)))
 
+(def a (first wires))
+(def b (second wires))
+(def intersections (hashset/intersection (set a) (set b)))
+
 (defn part1 []
-  (->> (wire-intersection (first wires) (second wires))
+  (->> intersections
        (map #(map abs %))
        (map #(reduce + %))
        sort
        second))
 
 (defn part2 []
-  (let [a (first wires) b (second wires)
-        wla (wire->locations a) wlb (wire->locations b)]
-        (->> (wire-intersection a b)
-            (map #(+ (wla %) (wlb %)))
-            sort
-            second)))
+  (->> intersections
+       (map #(+ (.indexOf a %) (.indexOf b %)))
+       sort
+       second))
 
 (defn -main
   [& args]
