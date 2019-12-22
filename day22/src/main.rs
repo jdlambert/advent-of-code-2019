5d57b94cee0da6375219d79ecbeedefb879e782b
Jacob Lambert <jacob.lambert@gmail.com>
2019-12-22 13:45:05 -0500

    Add finished day 22

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -3,24 +3,61 @@ extern crate regex;
 use regex::Regex;
 
 enum Action {
-    Deal(isize),
-    Cut(isize),
+    Deal(i128),
+    Cut(i128),
     Stack,
 }
 
-fn part1(actions: &Vec<Action>) -> isize {
+fn part1(actions: &Vec<Action>) -> i128 {
     let len = 10007;
     actions.iter().fold(2019, |i, action| match action {
+        Action::Stack => len - 1 - i,
         Action::Deal(n) => (i * n),
         Action::Cut(n) if *n >= 0 => (i - n + len),
         Action::Cut(n) if *n < 0 => (i + n.abs()),
-        Action::Stack => len - 1 - i,
         _ => unreachable!(),
     } % len)
 }
 
-fn part2() -> &'static str {
-    "nothing yet"
+fn mod_exp(base: i128, exponent: i128, modulus: i128) -> i128 {
+    let mut result = 1;
+    let mut base = base % modulus;
+    let mut exponent = exponent;
+
+    loop {
+        if exponent <= 0 {
+            break result;
+        }
+        if exponent % 2 == 1 {
+            result = (result * base) % modulus;
+        }
+        exponent = exponent >> 1;
+        base = (base * base) % modulus;
+    }
+}
+
+fn part2(actions: &Vec<Action>) -> i128 {
+    const LEN: i128 = 119_315_717_514_047;
+    const TIMES: i128 = 101_741_582_076_661;
+
+    let (offset_diff, increment_mul) =
+        actions
+            .clone()
+            .iter()
+            .fold((0, 1), |(offset, increment), action| {
+                let (new_offset, new_increment) = match action {
+                    Action::Stack => (offset - increment, -increment),
+                    Action::Cut(n) => (offset + increment * *n, increment),
+                    Action::Deal(n) => (offset, increment * mod_exp(*n, LEN - 2, LEN)),
+                };
+                (new_offset % LEN, new_increment % LEN)
+            });
+
+    let increment = mod_exp(increment_mul, TIMES, LEN);
+    let offset = offset_diff * (1 - mod_exp(increment_mul, TIMES, LEN)) % LEN;
+    let offset = offset * mod_exp(1 - increment_mul, LEN - 2, LEN) % LEN;
+
+    (offset + increment * 2020) % LEN
 }
 
 fn main() {
@@ -46,5 +83,5 @@ fn main() {
         })
         .collect();
     println!("Part 1: {}", part1(&actions));
-    println!("Part 2: {}", part2());
+    println!("Part 2: {}", part2(&actions));
 }
