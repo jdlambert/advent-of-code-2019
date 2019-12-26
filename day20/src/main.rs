b539b53cbf98de9edb3ce793adf43778bc7187de
Jacob Lambert <jacob.lambert@gmail.com>
2019-12-25 20:18:10 -0500

    Add day 20 part 1

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,17 +1,158 @@
-use std::fs;
+use std::{
+    cmp::Ordering,
+    collections::{BinaryHeap, HashMap, HashSet, VecDeque},
+    fs,
+};
 
-fn part1(data: &Vec<u32>) -> &str {
-    "nothing yet"
+type Pos = (isize, isize);
+type Map = HashMap<Pos, char>;
+type Graph = HashMap<String, HashSet<(String, usize)>>;
+
+fn get_adjacent((i, j): Pos, map: &Map) -> HashSet<(String, usize)> {
+    let mut set = HashSet::new();
+    let mut seen = HashSet::new();
+    let mut frontier = VecDeque::new();
+    frontier.push_back(((i + 1, j), 1));
+    frontier.push_back(((i - 1, j), 1));
+    frontier.push_back(((i, j + 1), 1));
+    frontier.push_back(((i, j - 1), 1));
+    seen.insert((i, j));
+
+    while frontier.len() > 0 {
+        let ((i, j), steps) = frontier.pop_front().unwrap();
+        if seen.contains(&(i, j)) {
+            continue;
+        }
+        seen.insert((i, j));
+        match map.get(&(i, j)).unwrap_or(&'#') {
+            '.' => {
+                frontier.push_back(((i + 1, j), steps + 1));
+                frontier.push_back(((i - 1, j), steps + 1));
+                frontier.push_back(((i, j + 1), steps + 1));
+                frontier.push_back(((i, j - 1), steps + 1));
+            }
+            c if c.is_ascii_uppercase() => {
+                if let Some(node) = get_portal((i, j), &map) {
+                    set.insert((node, steps - 1));
+                }
+            }
+            _ => (),
+        }
+    }
+    set
 }
 
-fn part2(data: &Vec<u32>) -> &str {
-    "nothing yet"
+fn get_portal((i, j): Pos, map: &Map) -> Option<String> {
+    let mut result = String::new();
+
+    if *map.get(&(i - 1, j)).unwrap_or(&' ') == '.' {
+        result.push(*map.get(&(i, j)).unwrap());
+        result.push(*map.get(&(i + 1, j)).unwrap());
+    } else if *map.get(&(i + 1, j)).unwrap_or(&' ') == '.' {
+        result.push(*map.get(&(i - 1, j)).unwrap());
+        result.push(*map.get(&(i, j)).unwrap());
+    } else if *map.get(&(i, j - 1)).unwrap_or(&' ') == '.' {
+        result.push(*map.get(&(i, j)).unwrap());
+        result.push(*map.get(&(i, j + 1)).unwrap());
+    } else if *map.get(&(i, j + 1)).unwrap_or(&' ') == '.' {
+        result.push(*map.get(&(i, j - 1)).unwrap());
+        result.push(*map.get(&(i, j)).unwrap());
+    }
+
+    if result.len() > 0 {
+        Some(result)
+    } else {
+        None
+    }
+}
+
+fn map_to_graph(map: Map) -> Graph {
+    let mut graph = Graph::new();
+
+    for (&pos, &c) in &map {
+        if c.is_ascii_uppercase() {
+            if let Some(node) = get_portal(pos, &map) {
+                let set = graph.entry(node).or_insert(HashSet::new());
+                for adjacent in get_adjacent(pos, &map) {
+                    set.insert(adjacent);
+                }
+            }
+        }
+    }
+
+    graph
+}
+
+#[derive(Eq, PartialEq)]
+struct State {
+    node: String,
+    cost: usize,
+}
+
+impl Ord for State {
+    fn cmp(&self, other: &State) -> Ordering {
+        other.cost.cmp(&self.cost)
+    }
+}
+
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &State) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+fn shortest_path(graph: Graph) -> usize {
+    let mut seen = HashSet::new();
+    let mut frontier = BinaryHeap::new();
+    frontier.push(State {
+        node: "AA".to_owned(),
+        cost: 0,
+    });
+
+    while let Some(State { node, cost }) = frontier.pop() {
+        if seen.contains(&node) {
+            continue;
+        }
+        seen.insert(node.clone());
+
+        if node == "ZZ" {
+            return cost;
+        }
+
+        for (adjacent, len) in graph.get(&node).unwrap() {
+            frontier.push(State {
+                node: adjacent.to_owned(),
+                cost: cost + len,
+            });
+        }
+    }
+
+    return std::usize::MAX;
+}
+
+fn part1(map: &Map) -> usize {
+    let graph = map_to_graph(map.clone());
+    println!("{:?}", graph);
+    shortest_path(graph) - 1
+}
+
+fn part2(map: &Map) -> usize {
+    2
 }
 
 fn main() {
     let content = fs::read_to_string("./input.txt").unwrap();
-    let lines = content.lines();
-    let data = lines.map(|x| x.parse::<u32>().unwrap()).collect();
-    println!("Part 1: {}", part1(&data));
-    println!("Part 2: {}", part2(&data));
+
+    let mut map = Map::new();
+
+    for (j, line) in content.lines().enumerate() {
+        let j = j as isize;
+        for (i, c) in line.chars().enumerate() {
+            let i = i as isize;
+            map.insert((i, j), c);
+        }
+    }
+
+    println!("Part 1: {}", part1(&map));
+    println!("Part 2: {}", part2(&map));
 }
